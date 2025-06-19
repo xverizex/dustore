8d4ca4051c480b3cf2e5d142d84a2987d6f797fb
AlexanderLivanov <help.underlinegroup@gmail.com>
2025-06-19 12:13:40 +0300

    oferta link and ver

--- swad/static/elements/footer.php
+++ swad/static/elements/footer.php
@@ -4,5 +4,8 @@
     <a href="https://vk.com/dgscorp">VKontakte (DGS)</a>
     <a href="https://t.me/dgscorp">Telegram (DGS)</a>
     <a href="https://t.me/dustore_official">Telegram (DUSTORE)</a>
+    <a href="/oferta.html">Публичная оферта</a>
     <p>DUSTORE (Dust Store) является собственностью Dust Studio.</p>
+    <!-- SemVer spec: status-global.design.tech#patch -->
+    <p>Версия платформы: prealpha-1.0.0#1</p>
 </div>
